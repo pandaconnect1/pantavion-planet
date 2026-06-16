06c4d9269f9d911538b46ab5ccae3c378d3199de
PANTAVION <infopandaconnect@gmail.com>
2026-06-16 06:04:07 +0300

    chore(water): connect final master DWG blob source

--- core/water/final-master-dwg-source.ts
+++ core/water/final-master-dwg-source.ts
@@ -1,4 +1,4 @@
-export const FINAL_MASTER_DWG_BLOB_URL = "";
+export const FINAL_MASTER_DWG_BLOB_URL = "https://trecvgzq9rkjoii8.private.blob.vercel-storage.com/pantavion/water/final-master/GEORGE_MAP_MASTER_B_C_FINAL.dwg";
 export const FINAL_MASTER_DWG_FILE_NAME = "GEORGE_MAP_MASTER_B_C_FINAL.dwg";
 export const FINAL_MASTER_DWG_SIZE_BYTES = 205877448;
 export const FINAL_MASTER_DWG_SHA256 = "0070db27b6b22cc3aa24353c9445f87910925b6d18bea27914c915da13bbc1d9";
