ee1c71e0672f84025820c6b6726c6c484d022b0f
pandaconnect1 <info.pandaconnect@gmail.com>
2026-09-24 06:36:36 +0300

    feat(water): add separate legacy Map B/C upload lane

---
+++ core/water/legacy-map-bc-source.ts
@@ -0,0 +1,7 @@
+export const LEGACY_MAP_BC_FILE_NAME = "GEORGE_MAP_MASTER_B_C_FINAL.dwg";
+export const LEGACY_MAP_BC_SIZE_BYTES = 205877448;
+export const LEGACY_MAP_BC_SHA256 = "0070db27b6b22cc3aa24353c9445f87910925b6d18bea27914c915da13bbc1d9";
+export const LEGACY_MAP_BC_DWG_HEADER = "AC1032";
+export const LEGACY_MAP_BC_STORAGE_BUCKET = "personal-media";
+export const LEGACY_MAP_BC_STORAGE_PATH =
+  `water-network-private/source-masters/legacy-map-bc/${LEGACY_MAP_BC_FILE_NAME}`;
